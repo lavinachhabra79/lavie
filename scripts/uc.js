fdece6b146c5111e95d6274317d605a596b9f099
Madhav Panchhiwala <madhav2panchhiwala@gmail.com>
2023-01-30 11:00:56 +0530

    first commit

---
+++ scripts/uc.js
@@ -0,0 +1,14 @@
+gsap.fromTo('#uc-section-main-img',3,
+    {
+        y : '-30px',
+        ease : "power1.inOut",
+        yoyo : true,
+        repeat : -1
+    },
+    {
+        y : '30px',
+        ease : "power1.inOut",
+        yoyo : true,
+        repeat : -1
+    }
+)
