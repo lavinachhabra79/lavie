9b9152dc2fddd4df019a1a76dbe421c55244dc47
Madhav Panchhiwala <madhav2panchhiwala@gmail.com>
2023-01-30 20:09:06 +0530

    hero section video section

---
+++ data/heroMarquee.js
@@ -0,0 +1,12 @@
+export const heroMarquee = [
+    'User experience',
+    'User interaction',
+    'User interviews',
+    'User research',
+    'Branding',
+    'SEO',
+    'Graphic design',
+    'Usability testing',
+    'Logo design'
+];
+
---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+import  { heroMarquee } from "../data/heroMarquee.js"
+
+// -------------------------------------------------Hero section marquee ------------------------------------------------- 
+const heroServicesContainer = document.querySelectorAll('.hero-section-bottom-services-container')
+
+console.log(heroServicesContainer)
+
+let marqueeElement = ''
+
+heroMarquee.forEach((service)=>{
+    marqueeElement += `<div class="marquee-item-service">${service} <span></span></div>`
+})
+
+heroServicesContainer.forEach((container)=>{
+    container.innerHTML = marqueeElement
+})
+
+
+// -------------------------------------------------Video section play button ------------------------------------------------- 
+
+gsap.from('.play-outer-circle',1,{
+    width : "220px",
+    height : "220px",
+    ease : 'ease.out',
+    yoyo : true,
+    repeat : -1
+})
